c6b0988cdfd732f92f4a54fd04bbb40d0e2ec1b5
yujimiya <yuji.miyamoto32@gmail.com>
2023-12-03 16:44:06 +0900

    修正完了しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は、全角ひらがな、全角カタカナ、漢字で入力して下さい" }
+  validates :password, presence: true, length{ minimum: 6 } 
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "は、全角ひらがな、全角カタカナ、漢字で入力して下さい" }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は、全角ひらがな、全角カタカナ、漢字で入力して下さい" }
   validates :last_name_kana, presence: true,   format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/, message: "は、全角カタカナで入力して下さい" }
   validates :first_name_kana, presence: true,   format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/, message: "は、全角カタカナで入力して下さい" }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,6 +32,12 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = '12345'
+        @user.password_confirmation = '12345'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
       it 'passwordが半角英数字混合でも5文字以下なら登録できない' do
         @user.password = 'abc12'
         @user.password_confirmation = 'abc12'
