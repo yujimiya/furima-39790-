fb2762310521c0000dc91f8a99ecc4a96d2a47e9
yujimiya <yuji.miyamoto32@gmail.com>
2023-12-10 20:26:24 +0900

    商品の表示順を指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:new, :create, :edit]
+  resources :items, only: [:new, :create, :edit, :index]
 end
