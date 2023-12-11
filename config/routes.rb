ba646a3ece39bbf13d5b8d829197439478a2673e
yujimiya <yuji.miyamoto32@gmail.com>
2023-12-11 20:29:52 +0900

    商品詳細ページが表示されるよう実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:new, :create, :edit, :index]
+  resources :items, only: [:new, :create, :edit, :index, :show]
 end
