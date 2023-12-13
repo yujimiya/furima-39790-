92e412f60668f494573ea41b75edee587659170c
yujimiya <yuji.miyamoto32@gmail.com>
2023-12-13 21:11:05 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,6 +35,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    if current_user.id == @item.user_id
+      @item.destroy
+    end
+    redirect_to root_path
+  end
 
   private
 
