5091a9748750e7745286fd24d350124687a53ae5
yujimiya <yuji.miyamoto32@gmail.com>
2023-12-25 21:59:30 +0900

    jsにてrenderについての修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,13 @@
-window.addEventListener('turbo:load', () => {
+const price =  () => {
   const priceInput = document.getElementById("item-price")
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("add-tax-price")
-    addTaxDom.innerHTML =Math.floor(inputValue * 0.10)
-    const addCalcDom = document.getElementById("profit")
-    addCalcDom.innerHTML = Math.floor(inputValue - (inputValue * 0.10))
+    addTaxDom.innerHTML =Math.floor(inputValue * 0.1)
+    const addprofit = document.getElementById("profit")
+    addprofit.innerHTML = Math.floor(inputValue - Math.floor(inputValue * 0.1))
     })
-  });
+  };
+
+  window.addEventListener("turbo:load", price)
+  window.addEventListener("turbo:render", price)
