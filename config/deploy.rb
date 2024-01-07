ac917599528d38ca12c5ef61858e8860ea942432
yujimiya <yuji.miyamoto32@gmail.com>
2024-01-07 16:36:40 +0900

    自動デプロイ設定完了

---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+lock '3.18.0'
+
+set :application, 'furima-39790-'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:yujimiya/furima-39790-.git'
+set :branch, 'main'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '3.2.0' 
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/yujimiya.pem'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
